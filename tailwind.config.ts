0e1a78345919226a14b8d8c75de1de744ee6baa8
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-23 02:39:52 +0100

    chore: scale and border radius

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,14 +30,15 @@ const config: Config = {
       },
     },
 
+    scale: {
+      "90": "0.990",
+      "95": "0.95",
+    },
+
     borderRadius: {
       DEFAULT: "8px",
       none: "0",
-      // sm: "0.125rem",
-      // md: "0.375rem",
-      // lg: "0.5rem",
-      // full: "9999px",
-      // large: "12px",
+      md: "12px",
     },
   },
   plugins: [],
