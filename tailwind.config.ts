67baadf5a021fa80238dab46421ce5319b684313
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-23 02:05:01 +0100

    chore: colors and border radius

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,8 @@ const config: Config = {
   ],
   theme: {
     colors: {
+      white: "#FFFFFF",
+      black: "#000000",
       danger: "#FF3939",
       primary: {
         DEFAULT: "#633CFF",
@@ -26,6 +28,16 @@ const config: Config = {
         disabled: "#633CFF",
         300: "#FAFAFA",
       },
+    },
+
+    borderRadius: {
+      DEFAULT: "8px",
+      none: "0",
+      // sm: "0.125rem",
+      // md: "0.375rem",
+      // lg: "0.5rem",
+      // full: "9999px",
+      // large: "12px",
     },
   },
   plugins: [],
