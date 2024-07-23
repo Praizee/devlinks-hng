c579320a289d8ecb1d5d23c87e6831ef1c4d2354
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-23 10:56:23 +0100

    update auth layout

--- app/_layouts/AuthLayout.tsx
+++ app/_layouts/AuthLayout.tsx
@@ -2,9 +2,9 @@ import type { AuthLayoutProps } from "@/app/interfaces";
 
 export const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <section className="bg-gray-300 lg:max-h-screen">
-      <div className="px-4 md:px-8 py-16 md:py-20 lg:flex justify-center">
-        {children}
+    <section className="bg-gray-300">
+      <div className="flex sm:justify-center sm:items-center h-screen w-full">
+        <div className="w-full sm:w-auto">{children}</div>
       </div>
     </section>
   );
