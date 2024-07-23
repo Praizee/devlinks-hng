dd2d09c892db8e07c72ac0e90449a1e6a8e206e0
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-23 23:16:26 +0100

    assets (icons)

---
+++ public/assets/svg/ChevronIcon.tsx
@@ -0,0 +1,18 @@
+import { Icon } from "./type";
+
+const ChevronIcon = ({ ...props }: Icon) => {
+  return (
+    <svg
+      {...props}
+      width="14"
+      height="9"
+      viewBox="0 0 14 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M13 8L7 2L1 8" stroke="#633CFF" stroke-width="2" />
+    </svg>
+  );
+};
+
+export default ChevronIcon;
