e31f9f22a5d3d5bbd740ade5618f719231259819
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-24 07:50:47 +0100

    feat: navbar

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -1,67 +1,77 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
-import Logo from "@/public/assets/images/Logo.png";
-import { IoClose, IoMenu } from "react-icons/io5";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import Logo from "@/public/assets/images/Logo.png";
+import MobileLogo from "@/public/assets/svg/mobile-logo.svg";
+import LinkIcon from "@/public/assets/svg/LinkIcon";
+import ProfileIcon from "@/public/assets/svg/ProfileIcon";
+import EyeIcon from "@/public/assets/svg/EyeIcon";
 
 const Navbar = () => {
-  const [state, setState] = useState(false);
+  const pathname = usePathname();
 
   return (
-    <section className=" p-[24px] bg-gray-300">
-      <nav className="w-full border-b md:border-0 sticky top-0 bg-white z-[999]">
-        <div className="items-center max-w-screen-2xl mx-auto md:flex">
-          <div className="flex items-center  px-4 md:px-8 justify-between py-3 md:py-5 md:block">
+    <section className="p-[24px] bg-gray-300">
+      <nav className="w-full border-b md:border-0 sticky top-0 bg-white rounded-md z-[999] p-[16px] pl-[24px]">
+        <div className="items-center max-w-screen-2xl mx-auto flex">
+          <div className="">
             <Link href="/">
-              <Image src={Logo} width={146} height={32} alt="DevLinks Logo" />
+              <Image
+                src={Logo}
+                width={146}
+                height={32}
+                alt="DevLinks Logo"
+                className="sm:block hidden"
+              />
             </Link>
-            <div className="md:hidden">
-              <button
-                className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
-                onClick={() => setState(!state)}
+            <Link href="/">
+              <Image
+                src={MobileLogo}
+                width={32}
+                height={32}
+                alt="DevLinks Logo"
+                className="block sm:hidden"
+              />
+            </Link>
+          </div>
+          <div className="flex-1 justify-self-center block justify-center text-center">
+            <div className="w-max flex lg:gap-[16px] mx-auto text-gray-100 font-medium">
+              <Link
+                href="/links"
+                className={`flex gap-[8px] items-center px-[16px] sm:px-[27px] py-[11px] rounded hover:text-primary duration-200 font-semibold active:scale-95 ${
+                  pathname === "/links" ? "text-primary bg-primary-200" : ""
+                }`}
               >
-                {state ? (
-                  <IoClose className="h-6 w-6" />
-                ) : (
-                  <IoMenu className="h-6 w-6" />
-                )}
-              </button>
+                <LinkIcon className="size-[20px]" />
+                <p className="hidden sm:block">Links</p>
+              </Link>
+              <Link
+                href="/profile"
+                className={`flex gap-[8px] items-center px-[16px] sm:px-[27px] py-[11px] rounded hover:text-primary duration-200 font-semibold active:scale-95 ${
+                  pathname === "/profile" ? "text-primary bg-primary-200" : ""
+                }`}
+              >
+                <ProfileIcon className="size-[20px]" />
+                <p className="hidden sm:block">Profile Details</p>
+              </Link>
             </div>
           </div>
-          <div
-            className={`flex-1 justify-self-center pb-3 md:block md:pb-0 md:mt-0 ${
-              state
-                ? "flex justify-center text-center pt-12 absolute h-screen w-full p-4 backdrop-blur-lg bg-black/70 text-white"
-                : "hidden"
-            }`}
+
+          {/* preview */}
+          <Link
+            href="/preview"
+            className="py-[11px] px-[16px] sm:px-[16px] flex gap-[8px] items-center font-semibold rounded text-base text-primary border border-primary hover:bg-primary-200 active:scale-95 duration-200"
           >
-            <div className="justify-center items-center space-y-8 md:flex md:gap-x-6 md:space-y-0">
-              <div className="md:text-[14px] text-[22px] text-white md:text-[#1E1C27] font-medium hover:text-primary duration-150 uppercase">
-                <Link
-                  href="/links"
-                  className="transition duration-300 flex bg-primary-200"
-                >
-                  Links
-                </Link>
-                <Link href="/profile" className="transition duration-300">
-                  Profile Details
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="hidden md:inline-block md:px-8">
-            <Link
-              href=""
-              className="py-[11px] px-[27px] font-semibold rounded text-base text-primary border border-primary hover:bg-primary-200 active:scale-95 duration-150"
-            >
-              Preview
-            </Link>
-          </div>
+            <EyeIcon className="size-[20px] block sm:hidden" />
+            <p className="hidden sm:block">Preview</p>
+          </Link>
         </div>
       </nav>
     </section>
   );
 };
+
 export default Navbar;
