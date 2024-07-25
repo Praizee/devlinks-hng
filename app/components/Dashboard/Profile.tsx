d13e0dd43610992ba464532c26aac0db4d78e321
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-25 09:54:17 +0100

    feat: split phone preview

--- app/components/Dashboard/Profile.tsx
+++ app/components/Dashboard/Profile.tsx
@@ -1,64 +1,14 @@
 import { BaseLayout } from "@/app/_layouts";
-import Image from "next/image";
 
-import Preview from "@/public/assets/svg/mobile-preview.svg";
 import ImageIcon from "@/public/assets/svg/ImageIcon";
-import Avatar from "@/public/assets/images/avatar.jpeg";
-import { PlatformLinks } from "@/app/components/common";
+import { PhonePreview } from "@/app/components/common";
 
 const Profile = () => {
   return (
     <BaseLayout>
       <section className="flex flex-col lg:flex-row gap-[24px]">
         <div className="hidden lg:flex justify-center items-center bg-white w-full max-w-[400px] xl:max-w-[560px] lg:min-h-[834px] p-[24px] rounded-[12px]">
-          <div className="relative flex justify-center items-center">
-            <Image src={Preview} alt="Phone Preview" className="select-none" />
-
-            <div className="flex flex-col gap-[46px] absolute bottom-8">
-              {/* profile image and details skeleton */}
-              {/* <div className="flex flex-col gap-[25px] items-center justify-center animate-pulse">
-                <div className="rounded-full size-[104px] bg-gray-400" />
-                <div className="flex flex-col gap-[13px] items-center w-max mx-auto">
-                  <span className="h-[26px] w-[160px] rounded bg-gray-400" />
-                  <span className="h-[18px] w-[72px] rounded bg-gray-400" />
-                </div>
-              </div> */}
-
-              {/* actual profile image and links */}
-              <div className="flex flex-col gap-[25px] items-center justify-center text-center">
-                <div className="rounded-full border-4 border-primary size-[104px] overflow-hidden">
-                  <Image
-                    src={Avatar}
-                    alt="Avatar"
-                    placeholder="blur"
-                    className="w-full h-full object-cover rounded-full"
-                  />
-                </div>
-
-                <span className="flex flex-col gap-[8px]">
-                  <h2 className="text-[32px] font-bold leading-[48px] text-gray">
-                    Ben Wright
-                  </h2>
-                  <p className="text-gray-100">ben@example.com</p>
-                </span>
-              </div>
-
-              {/* platform links skeleton */}
-              {/* <div className="flex flex-col gap-[15px] max-h-[280px] overflow-scroll animate-pulse pb-8">
-                <div className="w-[237px] h-[44px] rounded bg-gray-400" />
-                <div className="w-[237px] h-[44px] rounded bg-gray-400" />
-                <div className="w-[237px] h-[44px] rounded bg-gray-400" />
-                <div className="w-[237px] h-[44px] rounded bg-gray-400" />
-                <div className="w-[237px] h-[44px] rounded bg-gray-400" />
-                <div className="w-[237px] h-[44px] rounded bg-gray-400" />
-              </div> */}
-
-              {/* actual platform links */}
-              <div className="flex flex-col gap-[15px] max-h-[280px] overflow-scroll">
-                <PlatformLinks />
-              </div>
-            </div>
-          </div>
+          <PhonePreview />
         </div>
 
         <form className="flex flex-col divide-y-2 w-full">
--- app/components/common/index.ts
+++ app/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as CustomSelect } from "./CustomSelect";
 export { default as PlatformLinks } from "./PlatformLinks";
+export { default as PhonePreview } from "./PhonePreview";
