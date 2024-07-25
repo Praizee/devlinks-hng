f68435f2e29ec1589cac55981b8d3b352c269588
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-25 01:45:48 +0100

    feat: signup and login with firebase

--- app/components/Auth/CreateAccount.tsx
+++ app/components/Auth/CreateAccount.tsx
@@ -1,25 +1,67 @@
 "use client";
 
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/app/lib/firebaseConfig";
+import { AuthLayout } from "@/app/_layouts";
+import { toast, Toaster } from "react-hot-toast";
+
 import Link from "next/link";
 import Image from "next/image";
-import { AuthLayout } from "@/app/_layouts";
-
 import Logo from "@/public/assets/images/Logo.png";
 import EmailIcon from "@/public/assets/svg/EmailIcon";
 import PasswordIcon from "@/public/assets/svg/PasswordIcon";
 
+const schema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("Can't be empty"),
+  password: Yup.string()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Create strong password"
+    )
+    .required("Can't be empty"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), undefined], "Passwords must match")
+    .required("Can't be empty"),
+}).required();
+
+type FormData = Yup.InferType<typeof schema>;
+
 const CreateAccount = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      await createUserWithEmailAndPassword(auth, data.email, data.password);
+      router.push("/");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <AuthLayout>
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+        toastOptions={{
+          className:
+            "shadow-[#0000001A] lg:!min-w-[400px] font-semibold !bg-gray rounded-md !text-gray-300 px-[24px] py-[16px]",
+          style: {},
+        }}
+      />
+
       <div className="flex flex-col gap-[64px] sm:gap-[51px] w-full sm:w-[476px] p-[32px] sm:p-0">
         <Link href="/" className="w-max sm:mx-auto">
-          <Image
-            src={Logo}
-            alt="Logo"
-            // width={182}
-            // height={40}
-          />
+          <Image src={Logo} alt="Logo" />
         </Link>
 
         <div className="flex flex-col gap-[40px] sm:bg-white p-0 sm:p-[40px] rounded-md">
@@ -32,69 +74,61 @@ const CreateAccount = () => {
             </p>
           </span>
 
-          <form className="flex flex-col gap-[24px] text-gray">
-            <div className="flex flex-col gap-[4px]">
-              <label htmlFor="email" className="text-[12px] leading-[150%]">
-                Email address
-              </label>
-              <span className="relative">
-                <EmailIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
-
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="e.g. alex@email.com"
-                  required
-                  autoComplete="off"
-                  className="w-full pl-10 px-[16px] py-[12px] text-base text-gray placeholder:text-gray placeholder:opacity-[0.5] bg-transparent outline-none border focus:border-primary focus:shadow-input rounded"
-                />
-              </span>
-            </div>
-
-            <div className="flex flex-col gap-[4px]">
-              <label htmlFor="password" className="text-[12px] leading-[150%]">
-                Create password
-              </label>
-              <span className="relative">
-                <PasswordIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
-
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  required
-                  autoComplete="new-password"
-                  className="w-full pl-10 px-[16px] py-[12px] text-base text-gray placeholder:text-gray placeholder:opacity-[0.5] bg-transparent outline-none border focus:border-primary focus:shadow-input rounded"
-                />
-              </span>
-            </div>
-
-            <div className="flex flex-col gap-[4px]">
-              <label
-                htmlFor="confirm_password"
-                className="text-[12px] leading-[150%]"
-              >
-                Confirm password
-              </label>
-              <span className="relative">
-                <PasswordIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
-
-                <input
-                  id="confirm_password"
-                  name="confirm_password"
-                  type="password"
-                  placeholder="Enter your password"
-                  required
-                  autoComplete="new-password"
-                  className="w-full pl-10 px-[16px] py-[12px] text-base text-gray placeholder:text-gray placeholder:opacity-[0.5] bg-transparent outline-none border focus:border-primary focus:shadow-input rounded"
-                />
-              </span>
-            </div>
+          <form
+            className="flex flex-col gap-[24px]"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {["email", "password", "confirmPassword"].map((field) => (
+              <div key={field} className="flex flex-col gap-[4px]">
+                <label htmlFor={field} className="text-[12px] leading-[150%]">
+                  {field === "email"
+                    ? "Email address"
+                    : field === "password"
+                    ? "Create password"
+                    : "Confirm password"}
+                </label>
+                <span className="relative">
+                  {field === "email" && (
+                    <EmailIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
+                  )}
+                  {field === "password" && (
+                    <PasswordIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
+                  )}
+                  {field === "confirmPassword" && (
+                    <PasswordIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
+                  )}
+                  <input
+                    id={field}
+                    {...register(field as keyof FormData)}
+                    type={field === "email" ? "email" : "password"}
+                    placeholder={
+                      field === "email"
+                        ? "e.g. alex@email.com"
+                        : "At least 8 characters"
+                    }
+                    className={`w-full pl-10 px-[16px] py-[12px] text-base text-gray placeholder:text-gray placeholder:opacity-[0.5] bg-transparent outline-none border rounded ${
+                      errors[field as keyof FormData]
+                        ? "border-danger"
+                        : "border-gray"
+                    } focus:border-primary focus:shadow-input ${
+                      errors[field as keyof FormData]
+                        ? "ring-2 ring-danger"
+                        : ""
+                    }`}
+                  />
+                  {errors[field as keyof FormData] && (
+                    <p className="text-danger text-[12px] absolute right-3 inset-y-0 flex items-center">
+                      {errors[field as keyof FormData]?.message}
+                    </p>
+                  )}
+                </span>
+              </div>
+            ))}
 
             <p className="text-[12px] text-gray-100">
-              Password must contain at least 8 characters
+              Password must contain at least 8 characters, including one
+              uppercase letter, one lowercase letter, one number, and one
+              symbol.
             </p>
 
             <button
--- app/components/Auth/Login.tsx
+++ app/components/Auth/Login.tsx
@@ -1,28 +1,61 @@
 "use client";
 
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/app/lib/firebaseConfig";
+import { AuthLayout } from "@/app/_layouts";
+import { toast, Toaster } from "react-hot-toast";
+
 import Link from "next/link";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { AuthLayout } from "@/app/_layouts";
-
 import Logo from "@/public/assets/images/Logo.png";
 import EmailIcon from "@/public/assets/svg/EmailIcon";
 import PasswordIcon from "@/public/assets/svg/PasswordIcon";
 
+const schema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("Can't be empty"),
+  password: Yup.string()
+    .min(8, "At least 8 characters")
+    .required("Please check again"),
+}).required();
+
+type FormData = Yup.InferType<typeof schema>;
+
 const Login = () => {
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      router.push("/links");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <AuthLayout>
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+        toastOptions={{
+          className:
+            "shadow-[#0000001A] lg:!min-w-[400px] font-semibold !bg-gray rounded-md !text-gray-300 px-[24px] py-[16px]",
+          style: {},
+        }}
+      />
+
       <div className="flex flex-col gap-[64px] sm:gap-[51px] w-full sm:w-[476px] p-[32px] sm:p-0">
         <Link href="/" className="w-max sm:mx-auto">
-          <Image
-            src={Logo}
-            alt="Logo"
-            // width={182}
-            // height={40}
-          />
+          <Image src={Logo} alt="Logo" />
         </Link>
 
         <div className="flex flex-col gap-[40px] sm:bg-white p-0 sm:p-[40px] rounded-md">
@@ -35,47 +68,51 @@ const Login = () => {
             </p>
           </span>
 
-          <form className="flex flex-col gap-[24px]">
-            <div className="flex flex-col gap-[4px]">
-              <label htmlFor="email" className="text-[12px] leading-[150%]">
-                Email address
-              </label>
-              <span className="relative">
-                <EmailIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
-
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="e.g. alex@email.com"
-                  required
-                  autoComplete="off"
-                  className="w-full pl-10 px-[16px] py-[12px] text-base text-gray placeholder:text-gray placeholder:opacity-[0.5] bg-transparent outline-none border focus:border-primary focus:shadow-input rounded"
-                />
-              </span>
-            </div>
-
-            <div className="flex flex-col gap-[4px]">
-              <label htmlFor="password" className="text-[12px] leading-[150%]">
-                Password
-              </label>
-              <span className="relative">
-                <PasswordIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
-
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  required
-                  autoComplete="new-password"
-                  className="w-full pl-10 px-[16px] py-[12px] text-base text-gray placeholder:text-gray placeholder:opacity-[0.5] bg-transparent outline-none border focus:border-primary focus:shadow-input rounded"
-                />
-              </span>
-            </div>
+          <form
+            className="flex flex-col gap-[24px]"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {["email", "password"].map((field) => (
+              <div key={field} className="flex flex-col gap-[4px]">
+                <label htmlFor={field} className="text-[12px] leading-[150%]">
+                  {field === "email" ? "Email address" : "Password"}
+                </label>
+                <span className="relative">
+                  {field === "email" && (
+                    <EmailIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
+                  )}
+                  {field === "password" && (
+                    <PasswordIcon className="size-[16px] absolute left-3 inset-y-0 my-auto" />
+                  )}
+                  <input
+                    id={field}
+                    {...register(field as keyof FormData)}
+                    type={field === "email" ? "email" : "password"}
+                    placeholder={
+                      field === "email"
+                        ? "e.g. alex@email.com"
+                        : "Enter your password"
+                    }
+                    className={`w-full pl-10 px-[16px] py-[12px] text-base text-gray placeholder:text-gray placeholder:opacity-[0.5] bg-transparent outline-none border rounded ${
+                      errors[field as keyof FormData]
+                        ? "border-danger"
+                        : "border-gray"
+                    } focus:border-primary focus:shadow-input ${
+                      errors[field as keyof FormData]
+                        ? "ring-2 ring-danger"
+                        : ""
+                    }`}
+                  />
+                  {errors[field as keyof FormData] && (
+                    <p className="text-danger text-[12px] absolute right-3 inset-y-0 flex items-center">
+                      {errors[field as keyof FormData]?.message}
+                    </p>
+                  )}
+                </span>
+              </div>
+            ))}
 
             <button
-              onClick={() => router.push("/links")}
               type="submit"
               className="bg-primary hover:bg-primary-100 py-[11px] px-[27px] text-base text-white font-semibold active:scale-90 duration-200 rounded"
             >
